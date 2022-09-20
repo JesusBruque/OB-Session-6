f6339cc189ced0308a11928827b307485069d449
JesusBruque <bruke3_voleyutrera@hotmail.com>
2022-09-20 10:56:16 +0200

    commit

---
+++ src/components/Clock/Clock.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import "./clock.css";
+
+const Clock = () => {
+
+    const initialState = {
+        fecha: new Date(),
+        edad: 0,
+        nombre: "Martín",
+        apellidos: "San José"
+    };
+
+    const [user, setUser] = useState(initialState);
+
+    useEffect(() => {
+        const intervalAge = setInterval(() => {
+            actualiceUser();
+        }, 1000);
+        return () => {
+            clearInterval(intervalAge);
+        };
+    });
+
+    const actualiceUser = () => {
+        return setUser({
+            fecha: user.fecha,
+            edad: user.edad + 1,
+            nombre: user.nombre,
+            apellidos: user.apellidos,
+        });
+    };
+    return (
+        <div>
+            <h2>
+                Hora Actual:
+                {user.fecha.toLocaleTimeString()}
+            </h2>
+            <h3>{user.nombre} {user.apellidos}</h3>
+            <h1>Edad: {user.edad}</h1>
+        </div>
+    )
+};
+
+export default Clock;
